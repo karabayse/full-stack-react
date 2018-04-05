37423cd9bd58283963ff76ff0a327b614357023d
Kara Bayse <karabayse@gmail.com>
2018-04-05 08:58:44 -0500

    added get call for meetups

--- client_src/src/components/Meetups.js
+++ client_src/src/components/Meetups.js
@@ -1,6 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class Meetups extends Component {
+  constructor() {
+    super();
+    this.state = {
+      meetups: []
+    };
+  }
+
+  componentWillMount() {
+    console.log('in componentWillMount');
+    this.getMeetups();
+  }
+
+  getMeetups() {
+    axios.get('http://localhost:3000/api/meetups')
+      // Axios returns a promise
+      .then(response => {
+        console.log(response.data);
+        this.setState({meetups: response.data}, () => {
+          console.log(this.state);
+        });
+      });
+  }
+
   render() {
     return (
       <div>
--- client_src/src/components/Navbar.js
+++ client_src/src/components/Navbar.js
@@ -7,16 +7,18 @@ class Navbar extends Component {
       <div>
         <nav className="nav-extended">
             <div className="nav-wrapper">
-              <a href="/" className="brand-logo">MeetupsMN</a>
-              <a href="/main" data-activates="main-menu"
-                className="button-collapse show-on-large"><i className="fa fa-bars"></i></a>
-
+              <a href="/" className="brand-logo center">MeetupsMN</a>
+              <a data-activates="main-menu" className="button-collapse show-on-large">
+                  <i className="fa fa-bars"></i></a>
+              <li><Link to="/"><i className="fa fa-users"></i>Meetups</Link></li>
               <ul className="right hide-on-small-only">
-                <li><Link to="/"><i className="fa fa-users"></i>Meetups</Link></li>
+
               </ul>
             </div>
             <ul className="side-nav" id="main-menu">
-
+              <li><Link to="/"><i className="fa fa-users"></i>Meetups</Link></li>
+              <li><Link to="/meetups/add"><i className="fa fa-plus"></i>Add Meetup</Link></li>
+              <li><Link to="/about"><i className="fa fa-question-circle"></i>About</Link></li>
             </ul>
           </nav>
       </div>
